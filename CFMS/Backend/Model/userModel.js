308a4feb43ef734276c7074be5b49e92818b7800
Shreyanshmmgn <smmgn7934@gmail.com>
2021-03-26 23:37:13 +0530

    File renamed

---
+++ CFMS/Backend/Model/userModel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const userTemplate = new schema{
+    userName : {},
+    email : {},
+    password : {},
+};
+module.exports = 
+
