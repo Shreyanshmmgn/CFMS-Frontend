4b3841c6208b3ff942d361c452c25efc2b63eaf6
Shreyanshmmgn <smmgn7934@gmail.com>
2021-05-29 19:12:35 +0530

    Login perfectly working with all functions

--- CFMS-Frontend/Frontend/src/Login-Signup/login.js
+++ CFMS-Frontend/Frontend/src/Login-Signup/login.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import RegistrationForm from "../RegistrationForm/registrationForm.js";
-// import { Link } from "react-router-dom";
 import axios from "axios";
 import "../css/Login.css";
 import Avatar from "@material-ui/core/Avatar";
@@ -11,11 +9,11 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+axios.defaults.withCredentials = true;
 const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
@@ -91,87 +89,6 @@ const Login = () => {
   };
   const classes = useStyles();
   return (
-    // <div class="basic">
-    //   <div class="left-img">
-    //     <img
-    //       src="https://www.chitmonks.com/assets/images/about.png"
-    //       alt="CFMS"
-    //     />
-    //   </div>
-    //   <div class="card">
-    //     <div class="card-header">
-    //       <h2>Partner Login</h2>
-    //     </div>
-    //     <div class="card-body">
-    //       <form action="" onSubmit={submitData}>
-    //         <div class="input-group form-group">
-    //           <div class="input-group-prepend">
-    //             <span class="input-group-text">
-    //               <i class="fas fa-user"></i>
-    //             </span>
-    //           </div>
-    //           <input
-    //             type="text"
-    //             class="form-control"
-    //             placeholder="Email"
-    //             required
-    //             onChange={(e) => setEmail(e.target.value)}
-    //           />
-    //         </div>
-    //         <br></br>
-    //         <div class="input-group form-group">
-    //           <div class="input-group-prepend">
-    //             <span class="input-group-text">
-    //               <i class="fas fa-key"></i>
-    //             </span>
-    //           </div>
-    //           <input
-    //             type="password"
-    //             class="form-control"
-    //             placeholder="Password"
-    //             required
-    //             onChange={(e) => setPassword(e.target.value)}
-    //           />
-    //         </div>
-    //         <br></br>
-    //         <div class="row align-items-center remember">
-    //           <input type="checkbox" />
-    //           Remember Me
-    //         </div>
-    //         <h3 id="wrong-password"></h3>
-    //         <br></br>
-    //         <div class="form-group">
-    //           <input
-    //             type="submit"
-    //             value="Login"
-    //             class="btn float-right login_btn"
-    //           />
-    //         </div>
-    //       </form>
-    //     </div>
-    //     <div class="card-footer">
-    //       <div class="d-flex justify-content-center links">
-    //         <h5>
-    //           <b>Don't have an account?</b>
-    //         </h5>
-    //         &nbsp;&nbsp;
-    //         <h5>
-    //           <b>
-    //             <Link to="/Signup">Signup</Link>
-    //           </b>
-    //         </h5>
-    //       </div>
-    //       <div class="d-flex justify-content-center">
-    //         <h5>
-    //           <b>
-    //             <Link to="/forgotPassword">Forgot Password ?</Link>
-    //           </b>
-    //         </h5>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
@@ -181,7 +98,7 @@ const Login = () => {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            Login in
           </Typography>
           <form className={classes.form} noValidate onSubmit={submitData}>
             <TextField
@@ -223,12 +140,12 @@ const Login = () => {
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
+                <Link href="/forgotPassword" variant="body2">
                   Forgot password?
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/signUp" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
