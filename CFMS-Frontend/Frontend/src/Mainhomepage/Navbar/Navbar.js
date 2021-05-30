a7cad221db4be10e1320cbb5756f00c00540f191
Shreyanshmmgn <smmgn7934@gmail.com>
2021-05-30 23:53:22 +0530

    Testing Done

--- CFMS-Frontend/Frontend/src/Mainhomepage/Navbar/Navbar.js
+++ CFMS-Frontend/Frontend/src/Mainhomepage/Navbar/Navbar.js
@@ -9,18 +9,18 @@ function myFunction() {
 }
 
 // Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
-}
+};
 
 function Navbar() {
   return (
@@ -30,7 +30,7 @@ function Navbar() {
           style={{ height: "105px", width: "100px", paddingTop: "12px" }}
           src={image}
         />
-        <span style={{ fontSize: "17px" ,paddingRight: "20px" }}>
+        <span style={{ fontSize: "17px", paddingRight: "20px" }}>
           <br />
           <br />
           &nbsp;
@@ -44,47 +44,24 @@ function Navbar() {
         <img src={wallet} alt="Wallet" className="wallet"></img>
         <i className="fas fa-bell fa-lg">&nbsp;&nbsp;</i>
         <div className="dropdown">
-          <button onClick={myFunction} className="dropbtn">Shyam Yadav <img src={avatar} alt="Admin" width="40" />&nbsp;<span class="caret"></span></button>
+          <button onClick={myFunction} className="dropbtn">
+            Shyam Yadav <img src={avatar} alt="Admin" width="40" />
+            &nbsp;<span class="caret"></span>
+          </button>
           <div id="myDropdown" className="dropdown-content">
-          <a href="#"><i className="fas fa-user"></i> Profile</a>
-          <a href="#"><i className="fas fa-sliders-h"></i> Settings</a>
-          <a href="#"><i className="fas fa-sign-out-alt"></i> Signout</a>
+            <a href="#">
+              <i className="fas fa-user"></i> Profile
+            </a>
+            <a href="#">
+              <i className="fas fa-sliders-h"></i> Settings
+            </a>
+            <a href="#">
+              <i className="fas fa-sign-out-alt"></i> Signout
+            </a>
           </div>
         </div>
       </div>
-
-      <nav className="navbar1">
-        <input type="checkbox" id="check" />
-        <label for="check" class="checkbtn">
-          <i class="fas fa-bars"></i>
-        </label>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#about" className="btn-get-started scrollto">
-              About Chit Groups
-            </a>
-          </li>
-          <li>
-            <a href="#team" className="btn-get-started scrollto">
-              Features
-            </a>
-          </li>
-          <li>
-            <a href="#contact" className="btn-get-started scrollto">
-              Your Details
-            </a>
-          </li>
-          <li>
-            <a href="#">Feedback</a>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 }
 export default Navbar;
-
-
