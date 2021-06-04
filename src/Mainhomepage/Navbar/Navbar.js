8442e8d01335f08ad3b8cf6a478598d2ea64c1a3
Shyam Yadav <shyamyadav21099@gmail.com>
2021-06-05 00:22:32 +0530

    checking dashboed in aws

--- src/Mainhomepage/Navbar/Navbar.js
+++ src/Mainhomepage/Navbar/Navbar.js
@@ -1,63 +1,310 @@
-import React from 'react';
-import "./../../css/navbar.css";
-import image from "../../pics/logo.jpg";
-import wallet from "./../../pics/homepage/navbar/wallet.png";
-import avatar from "./../../pics/homepage/navbar/man.png";
+import React from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import SideBar from "./SideBar";
+import { useState } from "react";
+import Button from "@material-ui/core/Button";
 
+import SmallNav from "./SmallNav";
+import PublicChit from './../Chitfunds/PublicChit';
 
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "500px",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+ 
+}));
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-}
-const Navbar = () => {
+export default function Navbar() {
+  const classes = useStyles();
+  const [ currentValue, setcurrentValue] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+          <Badge badgeContent={11} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
-    <header id="header" className="fixed-top">
-      <div className="main-div1">
+    <div className={classes.grow}>
+      <AppBar style={{ backgroundColor: "#455a64" }} position="static">
+        <Toolbar>
+          <SideBar />
 
-        <img
-          style={{ height: "105px", width: "100px", paddingTop: "12px" }}
-          src={image}
-        />
-        <span style={{ fontSize: "17px", paddingRight: "20px" }}>
-          <br />
-          <br />
-          &nbsp;
-          <b>कमिटी करो </b>
-        </span>
-        <input
-          className="search-bar"
-          type="text"
-          placeholder="Search.."
-        ></input>
-        <img src={wallet} alt="Wallet" className="wallet"></img>
-        <i className="fas fa-bell fa-lg">&nbsp;&nbsp;</i>
-        <div className="dropdown">
-          <button onClick={myFunction} className="dropbtn">
-            Shyam Yadav <img src={avatar} alt="Admin" width="40" />
-            &nbsp;<span class="caret"></span>
-          </button>
-          <div id="myDropdown" className="dropdown-content">
-          <a href="#"><i className="fas fa-user"></i> Profile</a>
-          <a href="#"><i className="fas fa-sliders-h"></i> Settings</a>
-          <a href="#"><i className="fas fa-sign-out-alt"></i> Signout</a>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Commiittee Kro
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
           </div>
-        </div>
-      </div>
-    </header>
+          <div className = "spacing">
+            <Button style={{marginLeft: '40px'}}  variant="contained" color="#fafafa" onClick={() => {setcurrentValue(false)}} disableElevation>
+              Public Chit
+            </Button>
+            <Button style={{marginLeft: '20px'}} variant="contained" color="secondary" onClick={() => {setcurrentValue(true)}} disableElevation>
+              Private Chit
+            </Button>
+      
+          </div>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <IconButton aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton aria-label="show 17 new notifications" color="inherit">
+              <Badge badgeContent={17} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+      {
+
+       currentValue ? <SmallNav/> : <PublicChit />
+ }
+    </div>
+     
   );
 }
-export default Navbar;
+
+// import React from 'react';
+// import "./../../css/navbar.css";
+// import image from "../../pics/logo.jpg";
+// import wallet from "./../../pics/homepage/navbar/wallet.png";
+// import avatar from "./../../pics/homepage/navbar/man.png";
+
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+// window.onclick = function (event) {
+//   if (!event.target.matches(".dropbtn")) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains("show")) {
+//         openDropdown.classList.remove("show");
+//       }
+//     }
+//   }
+// }
+// const Navbar = () => {
+
+//   return (
+//     <header id="header" className="fixed-top">
+//       <div className="main-div1">
+
+//         <img
+//           style={{ height: "105px", width: "100px", paddingTop: "12px" }}
+//           src={image}
+//         />
+//         <span style={{ fontSize: "17px", paddingRight: "20px" }}>
+//           <br />
+//           <br />
+//           &nbsp;
+//           <b>कमिटी करो </b>
+//         </span>
+//         <input
+//           className="search-bar"
+//           type="text"
+//           placeholder="Search.."
+//         ></input>
+//         <img src={wallet} alt="Wallet" className="wallet"></img>
+//         <i className="fas fa-bell fa-lg">&nbsp;&nbsp;</i>
+//         <div className="dropdown">
+//           <button onClick={myFunction} className="dropbtn">
+//             Shyam Yadav <img src={avatar} alt="Admin" width="40" />
+//             &nbsp;<span class="caret"></span>
+//           </button>
+//           <div id="myDropdown" className="dropdown-content">
+//           <a href="#"><i className="fas fa-user"></i> Profile</a>
+//           <a href="#"><i className="fas fa-sliders-h"></i> Settings</a>
+//           <a href="#"><i className="fas fa-sign-out-alt"></i> Signout</a>
+//           </div>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
+// export default Navbar;
