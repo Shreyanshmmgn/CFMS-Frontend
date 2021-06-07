059f85201523538855adc4ac11e417e01a361983
Shyam Yadav <shyamyadav21099@gmail.com>
2021-06-07 13:57:13 +0530

    error page design done

--- src/Login-Signup/signup.js
+++ src/Login-Signup/signup.js
@@ -11,6 +11,7 @@ import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import img1 from "./../pics/computer.png";
 import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
@@ -18,7 +19,8 @@ const useStyles = makeStyles((theme) => ({
     height: "100vh",
   },
   image: {
-    backgroundImage: "url(https://www.chitmonks.com/assets/images/about.png)",
+    backgroundImage: `url(${img1})`,
+    width: "50%",
     backgroundRepeat: "no-repeat",
     backgroundColor:
       theme.palette.type === "light"
@@ -98,7 +100,7 @@ const Signup = () => {
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={false} sm={4} md={7} style={{backgroundImage: `url(${img1})`}} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
