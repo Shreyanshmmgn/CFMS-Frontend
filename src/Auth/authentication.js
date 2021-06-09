b21d11a0e8572e7d37b9151f6a51b4a7a997a13a
Shyam Yadav <shyamyadav21099@gmail.com>
2021-06-10 00:15:40 +0530

    Pending commit

--- src/Auth/authentication.js
+++ src/Auth/authentication.js
@@ -1,24 +1,13 @@
-// import { useCookies } from "react-cookie";
 import axios from "axios";
-// import { useState } from "react";
 
 const useAuthServices = () => {
-  // const [cookies, setCookies] = useCookies({});
-
-  //* Function to save cookies with relevent data
-  // const setCookie = (token) => {
-  //   console.log("Token : ", token);
-  //   // const expires = moment().add(token.expires);
-  //   // setCookies("Expires", expires, { path: "/" });
-  //   // setCookies("Token", token.token, { path: "/" });
-  // };
-
-  //* Function to logout cookies
-
+ 
   const logout = () => {
     console.log("Logout Called ");
-    axios.post(process.env.REACT_APP_BACKEND_URL + "/logout").then((res) => {
+    
+      axios.post(process.env.REACT_APP_BACKEND_URL + "/logout").then((res) => {
       console.log("Logout Successfully hurrrh");
+
     });
   };
 
