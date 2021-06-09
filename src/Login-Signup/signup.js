8fb94377185578aa18537072b7fe16b92fd88041
Shyam Yadav <shyamyadav21099@gmail.com>
2021-06-09 22:10:04 +0530

    all updated

--- src/Login-Signup/signup.js
+++ src/Login-Signup/signup.js
@@ -20,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
     height: "100vh",
   },
   image: {
-    backgroundImage: `url(${img1})`,
+    backgroundImage: "url(https://www.chitmonks.com/assets/images/about.png)",
     width: "50%",
     backgroundRepeat: "no-repeat",
 
@@ -149,7 +149,7 @@ const Signup = () => {
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} style={{backgroundImage: `url(${img1})`}} className={classes.image} />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
