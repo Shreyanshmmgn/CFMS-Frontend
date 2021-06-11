eb13440c8759df28988b6a2b1943808d7dede12e
Shreyanshmmgn <smmgn7934@gmail.com>
2021-06-11 21:12:57 +0530

    working on members

--- src/Mainhomepage/Members.js
+++ src/Mainhomepage/Members.js
@@ -1,38 +1,44 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import axios from "axios";
 
 const useRowStyles = makeStyles({
   root: {
-    '& > *': {
-      borderBottom: 'unset',
+    "& > *": {
+      borderBottom: "unset",
     },
   },
 });
 
-function createData(name, role,  memberSince, avgBid, watt) {
+function createData(name, Role, memberSince, email) {
   return {
     name,
-    role,
+    Role,
     memberSince,
-    avgBid,
-    watt,
+    email,
+    // watt,
     history: [
-      { location: 'Gurgaon', amountTransacted: 31000, age: 21, activeGroups: 4, hasReferred: 3 },
-     
+      {
+        location: "Gurgaon",
+        amountTransacted: 31000,
+        age: 21,
+        activeGroups: 4,
+        hasReferred: 3,
+      },
     ],
   };
 }
@@ -41,22 +47,25 @@ function Row(props) {
   const { row } = props;
   const [open, setOpen] = React.useState(false);
   const classes = useRowStyles();
-
+  console.log("Row : ", row);
   return (
     <React.Fragment>
       <TableRow className={classes.root}>
         <TableCell>
-          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
         </TableCell>
         <TableCell component="th" scope="row">
-          {row.name}
+          {row.firstName}
         </TableCell>
-        <TableCell align="right">{row.role}</TableCell>
-        <TableCell align="right">{row.memberSince}</TableCell>
-        <TableCell align="right">{row.avgBid}</TableCell>
-        <TableCell align="right">{row.watt}</TableCell>
+        <TableCell align="right">{row.Role}</TableCell>
+        <TableCell align="right">{row.id}</TableCell>
+        <TableCell align="right">{row.emailId}</TableCell>
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
@@ -68,11 +77,11 @@ function Row(props) {
               <Table size="small" aria-label="purchases">
                 <TableHead>
                   <TableRow>
-                  <TableCell >Location</TableCell>
+                    <TableCell>Location</TableCell>
                     <TableCell>Amount Transacted So Far</TableCell>
                     <TableCell>Age</TableCell>
-                    <TableCell >Active Groups</TableCell>
-                    <TableCell >Has referred</TableCell>
+                    <TableCell>Active Groups</TableCell>
+                    <TableCell>Has referred</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
@@ -82,9 +91,9 @@ function Row(props) {
                         {historyRow.location}
                       </TableCell>
                       <TableCell>{historyRow.amountTransacted}</TableCell>
-                      <TableCell >{historyRow.age}</TableCell>
-                      <TableCell >{historyRow. activeGroups}</TableCell>
-                      <TableCell >{historyRow. hasReferred}</TableCell>
+                      <TableCell>{historyRow.age}</TableCell>
+                      <TableCell>{historyRow.activeGroups}</TableCell>
+                      <TableCell>{historyRow.hasReferred}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
@@ -100,14 +109,14 @@ function Row(props) {
 Row.propTypes = {
   row: PropTypes.shape({
     name: PropTypes.number.isRequired,
-    role: PropTypes.number.isRequired,
+    Role: PropTypes.number.isRequired,
     memberSince: PropTypes.number.isRequired,
     history: PropTypes.arrayOf(
       PropTypes.shape({
         amount: PropTypes.number.isRequired,
         customerId: PropTypes.string.isRequired,
         date: PropTypes.string.isRequired,
-      }),
+      })
     ).isRequired,
     name: PropTypes.string.isRequired,
     avgBid: PropTypes.number.isRequired,
@@ -116,40 +125,45 @@ Row.propTypes = {
 };
 
 const rows = [
-  createData('Shreyansh', "Creater", 6.0, 24, 4.0),
-  createData('Shyam', "Member", 9.0, 37, 4.3),
-  createData('Rohan', "Member", 16.0, 24, 6.0),
-  createData('Shubham',"Created", 3.7, 67, 4.3),
-  createData('Rohit', "Member", 16.0, 49, 3.9),
-  createData('Tarun', "Creator", 16.0, 49, 3.9),
-  createData('Tarun', "Creator", 16.0, 49, 3.9),
-  createData('Tarun', "Creator", 16.0, 49, 3.9),
-  createData('Tarun', "Creator", 16.0, 49, 3.9),
-  createData('Tarun', "Creator", 16.0, 49, 3.9),
-  createData('Tarun', "Creator", 16.0, 49, 3.9),
-  createData('Tarun', "Creator", 16.0, 49, 3.9),
-
-
-
+  createData("Shreyansh", "Creater", 6.0),
+  createData("Rohan", "Creater", 6.0),
 ];
 
 export default function CollapsibleTable() {
+  let members = [];
+  useEffect(() => {
+    axios
+      .post(process.env.REACT_APP_BACKEND_URL + `sendUserData`)
+      .then((res) => {
+        if (res.status === 200) {
+          members = res.data;
+          console.log("Members : ", members);
+        }
+      }).catch = (error) => {
+      console.log("Member not found  !!", error);
+    };
+  }, []);
   return (
     <TableContainer component={Paper}>
       <Table aria-label="collapsible table">
-        <TableHead style={{backgroundColor: '#424242'}}>
+        <TableHead style={{ backgroundColor: "#424242" }}>
           <TableRow>
             <TableCell />
-            <TableCell style={{color: 'white'}} >Name</TableCell>
-            <TableCell style={{color: 'white',}} align="right">Role</TableCell>
-            <TableCell style={{color: 'white'}} align="right">Member Since</TableCell>
-            <TableCell style={{color: 'white'}} align="right">Avg Bid%</TableCell>
-            <TableCell style={{color: 'white'}} align="right">WATT</TableCell>
+            <TableCell style={{ color: "white" }}>Name</TableCell>
+            <TableCell style={{ color: "white" }} align="right">
+              Role
+            </TableCell>
+            <TableCell style={{ color: "white" }} align="right">
+              Member Since
+            </TableCell>
+            <TableCell style={{ color: "white" }} align="right">
+              Email
+            </TableCell>{" "}
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <Row key={row.name} row={row} />
+            <Row key={row.firstName} row={row} />
           ))}
         </TableBody>
       </Table>
